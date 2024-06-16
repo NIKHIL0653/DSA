bef71e7d75703cab23396095ae63ba1a0f2b11ab
Nikhil Choudhary <nikhil0653@gmail.com>
2024-06-16 15:16:17 +0530

    number reversal
    added leetcode solution

--- Basic Maths/num_reversal.cpp
+++ Basic Maths/num_reversal.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
 int main(){
@@ -6,7 +7,7 @@ int main(){
     cin >> n;
     int rev_num = 0;
 
-    while(n>0){
+    while(n!=0){
     int last_digit = n % 10;
     rev_num = (rev_num * 10) + last_digit;
     n = n/10;
@@ -14,3 +15,17 @@ int main(){
 
     cout << rev_num;
 }
+
+// Leetcode Solution
+
+// class Solution {
+// public:
+//     int reverse(int x) {
+//         long ans = 0;
+//         while (x) {
+//             ans = ans * 10 + x % 10;
+//             x /= 10;
+//         }
+//         return (ans < INT_MIN || ans > INT_MAX) ? 0 : ans;
+//     }
+// };
