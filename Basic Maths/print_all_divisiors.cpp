6c13c3c55033bf1ec806b652bdb5b441eb80a3f1
Nikhil Choudhary <nikhil0653@gmail.com>
2024-06-21 08:44:22 +0530

    optimal code

--- Basic Maths/print_all_divisiors.cpp
+++ Basic Maths/print_all_divisiors.cpp
@@ -1,16 +1,42 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// void divisiors(int n){
+//     for(int i=1;i<=n;i++){
+//         if(n%i==0) cout<<i<<" ";
+//         }
+//         cout << endl;
+// }                            // Brute Force Method
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     divisiors(n);
+//     }
+
+// Optimal Method
+
+#include<bits/stdc++.h>
 using namespace std;
 
-void divisiors(int n){
-    for(int i=1;i<=n;i++){
-        if(n%i==0) cout<<i<<" ";
+void printdivisors(int n){
+    vector<int> ls;
+    for(int i=1; i<sqrt(n);i++)
+    {
+        if(n%i == 0){
+            ls.push_back(i);
+            if((n/i) != i){
+                ls.push_back(n/i);
+            }
         }
-        cout << endl;
+    }
+    sort(ls.begin(),ls.end());  // since the numbers will not be in order we sort them
+    for(auto it : ls) cout << it << " "; // printing them by iterating through the list
 }
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    divisiors(n);
-    }
-
+    cin >> n;
+    printdivisors(n);
+}
