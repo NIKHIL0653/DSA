6235f6f9963b811a57019afe9a73de0a860a4c3c
Nikhil Choudhary <nikhil0653@gmail.com>
2024-12-09 23:41:04 +0530

    learnt py vars

---
+++ Python/variables.py
@@ -0,0 +1,32 @@
+# Strings
+first_name = "Nikhil"
+food = "Pizza"
+
+print(f"Hello {first_name}")
+
+print(f"You Like {food}")
+
+#Note: f is known as "f-string" allows us to format
+#strings with variables. It is a feature of Python 3.6 and later.
+
+#Integers
+
+age = 23
+quantity = 3.5
+
+print(f"You are {age} years old")
+
+# Floats
+price = 10.99
+
+print(f"The price is $ {price}")
+
+# Booleans
+is_student = False
+
+# print(f"Are you a student?: {is_student}")
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
