d164e31152905058bc086a197aace466d0743d1a
Nikhil Choudhary <nikhil0653@gmail.com>
2024-12-13 15:09:18 +0530

    collections

---
+++ Python/Getting Started/collections.py
@@ -0,0 +1,49 @@
+# collection = single "variable"l used to store multiple values
+# List = [] ordered and changeable. Duplicates OK
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("apple"))
+# print(fruits.count("pineapple"))
+
+
+# Set = {} unordered and immutable, but Add/Remove OK. NO duplicates
+
+# fruits = {"apple", "orange", "banana", "coconut"}
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits) # no pineallple will give false
+
+# fruits.add("pineapple")
+# fruits.remove("apple")
+# fruits.pop()
+# fruits.clear()
+# print(fruits)
+
+
+# Tuple = () ordered and unchangeable. Duplicates OK. FASTER
+
+# fruits = ("apple", "orange", "banana", "coconut", "coconut")
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("pineapple" in fruits)
+
+# print(fruits.index("apple"))
+# print(fruits.count("coconut"))
+# print(fruits)
+
+# for fruit in fruits:
+#     print(fruit)
