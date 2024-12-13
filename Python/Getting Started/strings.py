30ac5ebfcac2230c4e0be7746775100d9b05a606
Nikhil Choudhary <nikhil0653@gmail.com>
2024-12-13 13:02:05 +0530

    strings in py

--- Python/Getting Started/strings.py
+++ Python/Getting Started/strings.py
@@ -1,6 +1,6 @@
 # String methods
 
-phone_number = input("Enter your number: ")
+# phone_number = input("Enter your number: ")
 
 # result = len(name) # returns length of string/array in int format
 # result = name. find("o") # returns first occurence of the character in string from start
@@ -11,6 +11,17 @@ phone_number = input("Enter your number: ")
 # result = name.isdigit() # checks if input is number or not return true/false
 # result = name.isalpha() # checks if input has only letters or not, will return in case of numbers (Bro123)/ space in between
 # result = phone_number.count("-") # counts numb of occurence of the char/init/etc specified
-result = phone_number.replace("-", "")
+#result = phone_number.replace("-", "")
 
-print(result)
+#print(result)
+
+credit_number = "1234-5678-9012-3456"
+
+# print(credit_number[0]) # prints thee 1st index of the number
+print(credit_number[:4]) # prints from 0th to idex specified
+# print(credit_number[5:9]) # prints numb between index
+# print(credit_number [5:]) # Prints from index to end
+# print(credit_number[-1]) # Prints digit from end -1 = 6 here
+# print(credit number[::2]) # Returns every 2nd digit
+
+## Checkout chapter 14 and 15 in bro code course for python
