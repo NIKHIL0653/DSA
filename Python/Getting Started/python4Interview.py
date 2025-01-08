d97378781e7193f65a5fb9cd2baf1ac8e559a0da
Nikhil Choudhary <nikhil0653@gmail.com>
2025-01-08 13:34:58 +0530

    python for coding interviews

---
+++ Python/Getting Started/python4Interview.py
@@ -0,0 +1,24 @@
+# python is dynamically typed
+    # hence no need to specify data type
+# python is case sensitive
+# python has built in support for lists, dictionaries, sets, etc.
+
+#Note:
+n = 0
+n = n + 1 # allowed
+n += 1 # allowed
+n++ # not allowed
+
+# In python NULL is None
+
+n = 4
+n = None
+
+# In python we dont need parentheses in conditionals
+# but required in case of multi line conditional
+
+n, m = 1,2
+if((n>2 and
+    n != m)or n == 1):
+    n += 1
+ 
