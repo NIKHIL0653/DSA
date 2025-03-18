a5e49ac7c7943366777256376dc3f45afe68b3fb
Nikhil Choudhary <nikhil0653@gmail.com>
2025-03-18 15:29:28 +0530

    arrays java

---
+++ JAVA/DSA/Arrays.java
@@ -0,0 +1,14 @@
+package DSA;
+import java.util.*;
+
+public class Arrays {
+    public static void main(String args[]){
+        int[] marks = new int[3];
+        marks[0] = 98;
+        marks[1] = 99;
+        marks[2] = 91;
+        System.out.println(marks[0]);
+        // printing only marks will give us a mixture od class name and hash code (jargon)
+        // to print single element we use [] and specify the index number
+    }
+}
