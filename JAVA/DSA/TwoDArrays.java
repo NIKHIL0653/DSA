679a7514679d1e593a5ba191c02cf400c703d412
Nikhil Choudhary <nikhil0653@gmail.com>
2025-03-20 15:40:26 +0530

    java matrix

---
+++ JAVA/DSA/TwoDArrays.java
@@ -0,0 +1,14 @@
+package DSA;
+import.java.util*;
+
+public class TwoDArrays {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] numbers = int[rows][cols];
+
+        //taking input
+    }
+}
