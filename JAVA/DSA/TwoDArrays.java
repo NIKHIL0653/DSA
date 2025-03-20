7bb897301eaa25e7601d5ef252cbedbb9a07af71
Nikhil Choudhary <nikhil0653@gmail.com>
2025-03-20 15:44:33 +0530

    matrix

--- JAVA/DSA/TwoDArrays.java
+++ JAVA/DSA/TwoDArrays.java
@@ -1,5 +1,5 @@
 package DSA;
-import.java.util*;
+import java.util.*;
 
 public class TwoDArrays {
     public static void main(String args[]) {
@@ -7,8 +7,21 @@ public class TwoDArrays {
         int rows = sc.nextInt();
         int cols = sc.nextInt();
 
-        int[][] numbers = int[rows][cols];
+        int[][] numbers = new int[rows][cols];
 
         //taking input
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j < cols; j++){
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        // output
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j < cols; j++){
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
