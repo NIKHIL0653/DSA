f62966fece41288faf1881c701e024ccb87b2c4f
Nikhil Choudhary <nikhil0653@gmail.com>
2025-03-26 09:40:31 +0530

    sort

--- JAVA/DSA/InsertionSort.java
+++ JAVA/DSA/InsertionSort.java
@@ -1,5 +1,11 @@
 package DSA;
 
+/*
+There are 2 parts, sorted and unsorted part
+we pickup the element from the unsorted part and place it in the sorted
+part, and shift the adjacent elements forward filling up the blank space.
+ */
+
 public class InsertionSort {
     public static void printArray(int arr[]){
         for(int i = 0; i<arr.length; i++){
