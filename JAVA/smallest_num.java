1a1dcfe9c634da221d832cb8cdcb10fd2d3f8e1e
NIKHIL0653 <nikhil0653@gmail.com>
2025-04-25 16:51:51 +0530

    valid_pali

---
+++ JAVA/smallest_num.java
@@ -0,0 +1,16 @@
+// given a number we need to fin dthe smallest number
+// such that the product of digits is equal to given number
+
+/* 1. we need to find a nummber with least number of digits
+
+*/
+
+
+import java.util.*;
+
+public class smallest_num {
+	public static void main(String args[]){
+		
+	}
+	
+}
